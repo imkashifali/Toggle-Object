73f2af1f3bfe7460cd4e63c0c2cc94c44d8c80e8
kashifAli <shafatkashif@gmail.com>
2021-05-16 12:48:53 +0500

    first commit

---
+++ src/components/Student.js
@@ -0,0 +1,57 @@
+// import React from 'react'
+
+// function Student() {
+//     const [student, setStudent] = useState([
+//         {
+//           name: 'kashif',
+//           batch: 'Batch3',
+//           roll: '4125',
+//           class: 'Web And Mobile'
+//         },
+//         {
+//           name: 'Ali',
+//           batch: 'Batch3',
+//           roll: '4125',
+//           class: 'Web And Mobile'
+//         },
+//         {
+//           name: 'shahid',
+//           batch: 'Batch3',
+//           roll: '4125',
+//           class: 'Web And Mobile'
+//         },
+//       ]);
+    
+//       console.log(student);
+//     return (
+//         <div>
+//             <h2>List of Student</h2>
+//       <table>
+//         <tr>
+//           <th>Num</th>
+//           <th>Name</th>
+//           <th>Roll</th>
+//           <th>Class</th>
+//           <th>Batch</th>
+//         </tr>
+//         {
+//           student.map((stud, index) => {
+//             return (
+//               <tr>
+//                 <td>{index + 1}</td>
+//                 <td>{stud.name}</td>
+//                 <td>{stud.roll}</td>
+//                 <td>{stud.class}</td>
+//                 <td>{stud.batch}</td>
+//               </tr>
+//             )
+//           })
+//         }
+
+
+//       </table>
+//         </div>
+//     )
+// }
+
+// export default Student
